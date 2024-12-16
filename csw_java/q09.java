95497e39b7485e336da92183969ac71cd7a16805
Abhignya Ratnala <146984094+Abhiratnala@users.noreply.github.com>
2024-12-16 12:30:50 +0530

    Add files via upload

---
+++ csw_java/q09.java
@@ -0,0 +1,15 @@
+package csw_java;
+
+public class q09 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=7;
+		if(n%2==0)
+			System.out.println("even");
+		else
+			System.out.println("not even");
+
+	}
+
+}
